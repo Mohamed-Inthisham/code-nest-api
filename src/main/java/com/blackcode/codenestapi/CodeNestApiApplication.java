bbd6b412ba0eac6cd33b41ddd41ee61bc493cd9d
Mohamed-Inthisham <black.devil9b@gmail.com>
2024-03-17 11:30:45 +0530

    project initialized

---
+++ src/main/java/com/blackcode/codenestapi/CodeNestApiApplication.java
@@ -0,0 +1,13 @@
+package com.blackcode.codenestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeNestApiApplication.class, args);
+	}
+
+}
